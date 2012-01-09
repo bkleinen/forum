5ab4f98236d46e6712387d79e63c9b2a530cc650
B Kleinen <kleinen@infrastructure.de>
2012-01-09 15:46:27 +0100

    added topic_id to post

---
+++ db/migrate/20120109144445_add_topic_id_to_post.rb
@@ -0,0 +1,5 @@
+class AddTopicIdToPost < ActiveRecord::Migration
+  def change
+    add_column :posts, :topic_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120107173824) do
+ActiveRecord::Schema.define(:version => 20120109144445) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120107173824) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "topic_id"
   end
 
   create_table "topics", :force => true do |t|
