f6bef2feb30a0347d7ed9e0964d7bcd6c892906e
B Kleinen <kleinen@infrastructure.de>
2012-01-09 14:28:32 +0100

    started integration tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 
 describe "Posts" do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      visit posts_path
-      page.status_code.should be(200)
-    end
+  describe "creation" do
+    it "associates a topic with the post" 
   end
 end
--- spec/requests/topics_spec.rb
+++ spec/requests/topics_spec.rb
@@ -7,4 +7,9 @@ describe "Topics" do
       page.status_code.should be(200)
     end
   end
+  describe "on a single Topic Page" do
+    it "there is a button to create a post for this topic"
+    it "you get to the create Post page after clicking on that button" 
+  end
+  
 end
