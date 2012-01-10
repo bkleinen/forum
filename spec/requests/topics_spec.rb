45d12c2828686bcb0754ed3b98ebb42d3d005393
B Kleinen <kleinen@infrastructure.de>
2012-01-10 02:03:19 +0100

    added java script example

--- spec/requests/topics_spec.rb
+++ spec/requests/topics_spec.rb
@@ -5,7 +5,12 @@ describe "Topics" do
     it "has a link to New Topic" do
       visit topics_path
       click_link "New Topic"
-     page.should have_content("New topic")
+      page.should have_content("New topic")
+    end
+    it "supports js", :js => true do
+      visit topics_path
+      click_link "test js"
+      page.should have_content("js works")
     end
   end
   context "new page" do
@@ -17,18 +22,19 @@ describe "Topics" do
       page.should have_content "was successfully created"
     end
   end
-  describe "on a single Topic Page" do
+  describe "show page" do
     before(:each) do
       @topic = Topic.create(:title => 'An example Topic')
     end
-    it "there is a link to create a post for this topic" do
+    it "has a link to create a post for this topic" do
       visit topic_path(@topic)
       page.should have_link "Create new Post"
     end
-    it "you get to the create Post page after clicking on that button" do
+    it "link to create post takes you to the create Post page" do
       visit topic_path(@topic)
       click_link "Create new Post"
       page.should have_content "New post"
     end
   end
 end
+
