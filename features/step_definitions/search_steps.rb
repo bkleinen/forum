403beac9c3d9e97d92891857bfb3fa903fe030dd
B Kleinen <kleinen@infrastructure.de>
2012-01-16 23:30:07 +0100

    Step 10: Retrieving HTML Objects I

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -4,7 +4,9 @@ When /^I search for "([^"]*)"$/ do |query|
   click_button('Search')
 end
 
-Then /^the results should be:$/ do |table|
+Then /^the results should be:$/ do |expected_results|
+  results = [['content']] + page.all('ol.results li')
+  puts results.join("\n")
   # table is a Cucumber::Ast::Table
   pending # express the regexp above with the code you wish you had
 end
