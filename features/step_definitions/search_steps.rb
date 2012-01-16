a03c4f81d433b24604142db15e267fe2c4f2e7b2
B Kleinen <kleinen@infrastructure.de>
2012-01-16 22:33:48 +0100

    Step 04: Added pending Then Step

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -3,3 +3,8 @@ When /^I search for "([^"]*)"$/ do |query|
   fill_in('query', :with => query)
   click_button('Search')
 end
+
+Then /^the results should be:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
