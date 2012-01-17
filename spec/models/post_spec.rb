315fa2eab4cf22550301add822d878725faa684a
B Kleinen <kleinen@infrastructure.de>
2012-01-17 01:35:06 +0100

    created failing rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Post do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  it "finds posts with content" do
+    Post.create(:title => "find",:content => "contains IT")
+    results = Post.containing("IT")
+    results.length.should be 1
+  end
+  it "finds posts with content both in title and in content" do
+    Post.create(:title => "find",:content => "contains IT")
+    Post.create(:title => "find IT etc",:content => "contains")
+    results = Post.containing("IT")
+    results.length.should be 2
+  end
 end
