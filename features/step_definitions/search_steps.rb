2a89608dd5028d2c4e4c9191d7c52cba70585de7
B Kleinen <kleinen@infrastructure.de>
2012-01-17 01:42:15 +0100

    and cleaned up the feature

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -8,11 +8,6 @@ Then /^the results should be:$/ do |expected_results|
   results = page.all('ol.results li').map do |li|
     [li.text]
   end
-  puts " ==== currently in results:"
-  # puts results.join("\n")
   # table is a Cucumber::Ast::Table - it has a diff! method
-  puts results.inspect
-  puts expected_results.inspect
   expected_results.diff!(results)
-  pending # express the regexp above with the code you wish you had
 end
